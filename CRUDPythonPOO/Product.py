aaad5a08a6153e535db832c3170845dac1435ee5
emilio barrera <barrera_emilio@yahoo.es>
2023-02-01 17:58:08 +0000

    done

---
+++ CRUDPythonPOO/Product.py
@@ -0,0 +1,15 @@
+class Porduct:
+	def __init__(self, name, price):
+		self.Name = name
+		self.Price = price
+
+		
+	def updatePrice(self, price):
+		self.Price = price
+
+
+	def __str__(self): return f"{self.name}: {self.Price}"
+
+
+		 
+     
